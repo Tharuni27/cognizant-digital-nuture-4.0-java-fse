7456cddd83631ab02321f3e3edf18c1076fd5002
Tharuni <allatharuni2704@gmail.com>
2025-05-27 15:53:07 +0530

    Add files via upload

---
+++ module 3/core.java
@@ -0,0 +1,11 @@
+// File: core.java
+
+// Objective: Understand the basic structure of a Java program.
+// Task: Write a Java program that prints "Hello, World!" to the console.
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Display the message "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
